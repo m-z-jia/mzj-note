dfdd983eec2f67356784f48e96c518490eb2f040
mzj <3424126130@qq.com>
2026-04-09 19:11:38 +0800

    优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,57 +1,58 @@
 module.exports = {
+ base: '/mzj-note/',
   title: "蘑菇", // 网站的标题
   description: "我的个人博客", // 网站的描述
-  base: '/mzj-note/', // 部署到github相关的配置
+//   base: '/mzj-note/', // 部署到github相关的配置
   themeConfig: {
     logo: "/aseets/img/logo.png",
     //顶部导航栏配置
     nav: [
       { text: "首页", link: "/" },
 
-      {
-        text: "技术笔记",
-        items: [
-          {
-            text: "前端",
-            items: [
-              { text: "HTML & CSS", link: "/guide/前端学习笔记/" },
-              { text: "JavaScript", link: "/guide/japanese/" },
-              { text: "Vue", link: "/guide/japanese/" },
-            ],
-          },
+    //   {
+    //     text: "技术笔记",
+    //     items: [
+    //       {
+    //         text: "前端",
+    //         items: [
+    //           { text: "HTML & CSS", link: "/guide/前端学习笔记/" },
+    //           { text: "JavaScript", link: "/guide/japanese/" },
+    //           { text: "Vue", link: "/guide/japanese/" },
+    //         ],
+    //       },
 
-          {
-            text: "后端",
-            items: [
-              { text: "前端学习笔记", link: "/guide/前端学习笔记/" },
-              { text: "Java学习笔记", link: "/guide/japanese/" },
-            ],
-          },
-        ],
-      },
+    //       {
+    //         text: "后端",
+    //         items: [
+    //           { text: "前端学习笔记", link: "/guide/前端学习笔记/" },
+    //           { text: "Java学习笔记", link: "/guide/japanese/" },
+    //         ],
+    //       },
+    //     ],
+    //   },
 
-      {
-        text: "常见问题",
-        items: [
-          { text: "Chinese", link: "/language/chinese/" },
-          { text: "Japanese", link: "/language/japanese/" },
-        ],
-      },
-      {
-        text: "面试常问",
-        items: [
-          { text: "Chinese", link: "/language/chinese/" },
-          { text: "Japanese", link: "/language/japanese/" },
-        ],
-      },
-      {
-        text: "开源项目",
-        items: [
-          { text: "Chinese", link: "/language/chinese/" },
-          // { text: 'Japanese', link: '/language/japanese/' }
-        ],
-      },
-      { text: "关于我", link: "/about/about.md" },
+    //   {
+    //     text: "常见问题",
+    //     items: [
+    //       { text: "Chinese", link: "/language/chinese/" },
+    //       { text: "Japanese", link: "/language/japanese/" },
+    //     ],
+    //   },
+    //   {
+    //     text: "面试常问",
+    //     items: [
+    //       { text: "Chinese", link: "/language/chinese/" },
+    //       { text: "Japanese", link: "/language/japanese/" },
+    //     ],
+    //   },
+    //   {
+    //     text: "开源项目",
+    //     items: [
+    //       { text: "Chinese", link: "/language/chinese/" },
+    //       // { text: 'Japanese', link: '/language/japanese/' }
+    //     ],
+    //   },
+    //   { text: "关于我", link: "/about/about.md" },
     ],
     sidebar: [
       {
@@ -60,14 +61,10 @@ module.exports = {
         collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 1, // 可选的, 默认值是 1
         children: [
-          {
-            title: "HTML基础学习",
-            path: "/guide/前端学习笔记/01html基础.md",
-          },
-          {
-            title: "bas",
-            path: "/guide/前端学习笔记/00html基础.md",
-          },
+        //   {
+        //     title: "bas",
+        //     path: "/guide/前端学习笔记/00html基础.md",
+        //   },
         ],
       },
       {
